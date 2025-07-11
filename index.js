b05f3305459d8b4c8fb3c6641cbab7022134a5c8
Sol1de <olivier.delmas@edu.devinci.fr>
2025-07-11 10:18:26 +0200

    added chat feature

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
         console.log('user disconnected');
     });
     socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
+        io.emit('chat message', msg);
     });
 });
 
